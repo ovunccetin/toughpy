f07f669a05f70d4233cf5300b78bc256c24c0374
ovunccetin <ovunccetin@gmail.com>
2018-12-07 11:38:28 +0300

    initial setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='toughpy',
+    version='0.1',
+    author='Ovunc Cetin',
+    author_email='ovunccetin@gmail.com',
+    description='A fault tolerance library designed for Python.',
+    license='Apache',
+    packages=['toughpy'],
+    extras_require={
+        'dev': [
+            'pytest>=3'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Topic :: Utilities',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest']
+)
---
+++ tests/conftest.py
@@ -0,0 +1,78 @@
+import inspect
+
+
+def pytest_itemcollected(item):
+    parent = item.parent.obj
+    node = item.obj
+
+    parent_label = _get_parent_label(parent)
+    test_label = _get_test_label(node)
+    if test_label:
+        item._nodeid = bold(magenta(' + ')) + bold(parent_label) + ': ' + cyan(test_label)
+
+
+def _get_parent_label(parent):
+    if parent.__doc__:
+        label = parent.__doc__
+    elif inspect.ismodule(parent):
+        label = parent.__name__.split('.')[-1]
+    elif isinstance(parent, object):
+        cls = parent.__class__
+        mdl = cls.__module__.split('.')[-1]
+        label = mdl + '.' + cls.__name__
+    else:
+        label = "<unknown>"
+
+    label = label.replace('Test', '').replace('test_', '')
+
+    return label
+
+
+def _get_test_label(node):
+    label = node.__doc__ if node.__doc__ else node.__name__
+
+    lines = filter(
+        lambda l: len(l) > 0,
+        [line.strip() for line in label.splitlines()]
+    )
+
+    first = True
+    line_list = []
+    for line in lines:
+        if first:
+            line_list.append(line)
+            first = False
+        else:
+            line_list.append('   ' + line)
+
+    return '\n'.join(line_list)
+
+
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+GRAY = '\033[90m'
+
+BOLD = '\033[1m'
+RESET = '\033[0m'
+
+
+def colorize(text, color):
+    return color + text + RESET
+
+
+def magenta(text):
+    return colorize(text, MAGENTA)
+
+
+def cyan(text):
+    return colorize(text, CYAN)
+
+
+def bold(text):
+    return colorize(text, BOLD)
